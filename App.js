db6cd8b1abf9b0a62c5a3ab91518eb9d38da95e6
appitsolution <applicationitsolution@gmail.com>
2023-05-10 16:22:30 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,47 @@
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { useFonts } from "expo-font";
+import Home from "./screen/home";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    // "Comfortaa-Regular": require("./assets/fonts/Comfortaa-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="home"
+          component={Home}
+          options={{
+            animationEnabled: false,
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+      <StatusBar style="auto" />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ screen/home.js
@@ -0,0 +1,241 @@
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Image,
+} from "react-native";
+import normal from "../style/normal";
+import Logo from "../components/Logo";
+import Swiper from "react-native-swiper";
+import { useState } from "react";
+import catalog from "../assets/catalog.jpg";
+import CatalogPlus from "../assets/Icons/CatalogPlus";
+
+const sortButtons = [
+  { text: "Всі", active: true },
+  { text: "Eur/Epal", active: false },
+  { text: "800x120", active: false },
+  { text: "1000x1200", active: false },
+  { text: "Нові", active: false },
+  { text: "Пластикові", active: false },
+];
+
+const catalogData = [
+  {
+    title: "Європіддон б/в 1-й сорт, дерев’яний, світлий.",
+    desc: `Розміри: 800х1200х144(мм). Навантаження: до 2500кг. Маркування:
+        знаками EUR і відмітками IPPC`,
+  },
+  {
+    title: "Європіддон б/в 1-й сорт, дерев’яний, світлий.",
+    desc: `Розміри: 800х1200х144(мм). Навантаження: до 2500кг. Маркування:
+        знаками EUR і відмітками IPPC`,
+  },
+  {
+    title: "Європіддон б/в 1-й сорт, дерев’яний, світлий.",
+    desc: `Розміри: 800х1200х144(мм). Навантаження: до 2500кг. Маркування:
+        знаками EUR і відмітками IPPC`,
+  },
+  {
+    title: "Європіддон б/в 1-й сорт, дерев’яний, світлий.",
+    desc: `Розміри: 800х1200х144(мм). Навантаження: до 2500кг. Маркування:
+        знаками EUR і відмітками IPPC`,
+  },
+  {
+    title: "Європіддон б/в 1-й сорт, дерев’яний, світлий.",
+    desc: `Розміри: 800х1200х144(мм). Навантаження: до 2500кг. Маркування:
+        знаками EUR і відмітками IPPC`,
+  },
+];
+
+const Home = () => {
+  const [testTab, setTestTab] = useState(true);
+  return (
+    <View style={styles.home}>
+      <Logo />
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={{ flexDirection: "row", width: "100%", marginTop: 20 }}>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              paddingVertical: 12,
+
+              borderBottomColor: testTab ? "#272727" : "transparent",
+              borderBottomWidth: 4,
+            }}
+            onPress={() => setTestTab(true)}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                textTransform: "uppercase",
+                fontSize: 13,
+                lineHeight: 16,
+                fontWeight: 500,
+              }}
+            >
+              Поставка палет
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              flex: 1,
+              paddingVertical: 12,
+              borderBottomColor: !testTab ? "#272727" : "transparent",
+              borderBottomWidth: 4,
+            }}
+            onPress={() => {
+              setTestTab(false);
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                textTransform: "uppercase",
+                fontSize: 13,
+                lineHeight: 16,
+                fontWeight: 500,
+              }}
+            >
+              Викуп піддонів
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.sortButton}>
+          <FlatList
+            style={styles.sortButtonList}
+            data={sortButtons}
+            renderItem={({ item }) => {
+              return (
+                <TouchableOpacity
+                  style={{
+                    ...styles.sortButtonItem,
+                    backgroundColor: item.active ? "#272727" : "#F1F1F1",
+                  }}
+                >
+                  <Text
+                    style={{
+                      ...styles.sortButtonItemText,
+                      color: item.active ? "#ffffff" : "#272727",
+                    }}
+                  >
+                    {item.text}
+                  </Text>
+                </TouchableOpacity>
+              );
+            }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+
+        <View style={{ width: "100%", marginTop: 20, gap: 16 }}>
+          {catalogData.map((item) => (
+            <View
+              style={{
+                backgroundColor: "#FFFFFF",
+                shadowColor: "#000",
+                shadowOffset: {
+                  width: 0,
+                  height: 1,
+                },
+                shadowOpacity: 0.14,
+                shadowRadius: 1,
+                elevation: 2,
+                borderRadius: 4,
+                padding: 12,
+                paddingRight: 16,
+                flexDirection: "row",
+                gap: 14,
+              }}
+            >
+              <Image source={catalog} style={{ width: "40%" }} />
+              <View style={{ width: "60%" }}>
+                <Text
+                  style={{
+                    fontSize: 13,
+                    lineHeight: 16,
+                    fontWeight: "700",
+                    marginBottom: 5,
+                  }}
+                >
+                  {item.title}
+                </Text>
+                <Text
+                  style={{ fontSize: 13, lineHeight: 18, fontWeight: "400" }}
+                >
+                  {item.desc}
+                </Text>
+                <TouchableOpacity
+                  style={{ marginLeft: "auto", marginRight: 16 }}
+                >
+                  <CatalogPlus />
+                </TouchableOpacity>
+              </View>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  home: {
+    ...normal.containerTop,
+    backgroundColor: "#ffffff",
+  },
+  banner: {
+    position: "relative",
+    marginTop: 12,
+    height: "100%",
+    maxHeight: 142,
+  },
+  bannerItem: {
+    flex: 1,
+    flexDirection: "row",
+    columnGap: 4,
+    marginHorizontal: 4,
+  },
+  bannerFirst: {
+    width: "70%",
+    backgroundColor: "#D8D8D8",
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bannerLast: {
+    width: "30%",
+    backgroundColor: "#D8D8D8",
+    borderRadius: 8,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sortButton: {
+    marginTop: 30,
+  },
+  sortButtonList: {
+    width: "100%",
+    gap: 7,
+  },
+  sortButtonItem: {
+    marginHorizontal: 5,
+    borderRadius: 5,
+    overflow: "hidden",
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+  },
+
+  sortButtonItemText: {
+    fontSize: 14,
+    lineHeight: 20,
+    fontWeight: 500,
+    color: "#272727",
+  },
+});
+
+export default Home;
